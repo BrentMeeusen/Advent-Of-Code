3776d82eba73937bea22b4c9d97c876a4ec73d3a
BrentMeeusen <brentm@live.nl>
2025-12-03 11:09:28 +0100

    Add day 3 part 2 solution

--- 2025/python/day_3/main.py
+++ 2025/python/day_3/main.py
@@ -28,6 +28,37 @@ def find_max_joltage(bank: str) -> int:
     return l * 10 + r
 
 
+def find_max_overclocked_joltage(bank: str) -> int:
+
+    # Select none of the digits except for the last 12
+    selected = [(False if i < len(bank) - 12 else True) for i in range(len(bank))]
+
+    # For each of the 12 batteries
+    for i in range(12):
+        last_selected = int(bank[-1])
+        last_selected_index = -1
+
+        # Loop through all digits
+        for j in range(len(bank) - 1, -1, -1):
+            current = int(bank[j])
+
+            # If it's already selected, update selected pointers
+            if selected[j]:
+                last_selected = current
+                last_selected_index = j
+
+            # If it's not selected but the sum doesn't get worse by updating pointers, update
+            elif current >= last_selected:
+                last_selected = current
+                selected[last_selected_index] = False
+
+                last_selected_index = j
+                selected[last_selected_index] = True
+
+    # Collect selected digits and return as int
+    return int("".join([bank[i] if selected[i] else "" for i in range(len(bank))]))
+
+
 def solve_day_3_pt_1(battery_banks: list[str]) -> int:
     total_joltage = 0
 
@@ -37,7 +68,17 @@ def solve_day_3_pt_1(battery_banks: list[str]) -> int:
     return total_joltage
 
 
+def solve_day_3_pt_2(battery_banks: list[str]) -> int:
+    total_joltage = 0
+
+    for bank in battery_banks:
+        total_joltage += find_max_overclocked_joltage(bank)
+
+    return total_joltage
+
+
 if __name__ == "__main__":
     battery_banks = read_file("input.txt")
 
     print(solve_day_3_pt_1(battery_banks))
+    print(solve_day_3_pt_2(battery_banks))
