35fc0e78b5a02cb965b1416f4a4f3050aa41f9d2
BrentMeeusen <brentm@live.nl>
2025-12-05 13:53:38 +0100

    Add day 5 part 2 solution

--- 2025/python/day_5/main.py
+++ 2025/python/day_5/main.py
@@ -13,6 +13,45 @@ def solve_day_5_pt_1(ranges: list[range], ids: list[int]) -> int:
     return fresh_id_count
 
 
+def solve_day_5_pt_2(inputs: list[str]) -> int:
+    possible_fresh_id_count = 0
+
+    # Get all ranges from input
+    ranges = []
+    for row in inputs:
+        if row == "":
+            break
+
+        # Get min-max for current range
+        r_min, r_max = tuple(map(int, row.split("-")))
+
+        # Update min-max to only apply to IDs that aren't in a range already to prevent counting IDs multiple times,
+        # and exclude fully submerged ranges
+        excluded = 0
+        for range in ranges:
+            # If r_min partially overlaps range, exclude overlap
+            if range[0] <= r_min <= range[1]:
+                r_min = range[1] + 1
+
+            # If r_max partially overlaps range, exclude overlap
+            if range[0] <= r_max <= range[1]:
+                r_max = range[0] - 1
+
+            # If our (updated) range fully submerges the smaller range, exclude the smaller range
+            if r_min <= range[0] and r_max >= range[1]:
+                excluded += range[1] - range[0] + 1
+
+        # If there's at least one value to add, add it
+        if r_min <= r_max:
+            possible_fresh_id_count += r_max - r_min + 1
+            ranges.append((r_min, r_max))
+
+        # Remove fully submerged counts
+        possible_fresh_id_count -= excluded
+
+    return possible_fresh_id_count
+
+
 def split_inputs(inputs: list[str]) -> tuple[list[range], list[int]]:
     ranges = []
     ids = []
@@ -36,4 +75,4 @@ if __name__ == "__main__":
     ranges, ids = split_inputs(inputs)
 
     print(solve_day_5_pt_1(ranges, ids))
-    # print(solve_day_5_pt_2(diagram))
+    print(solve_day_5_pt_2(inputs))
